e8d66dfbf38b667da7ce69266bc20fa3f47965e1
Yen-Nan Lin <yennanlin.tw@gmail.com>
2016-12-05 16:18:29 +0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NUM_PATIENTS = 100
+NUM_PHYSICIANS = 100
+APPOINTMENTS = 500
+
+NUM_PATIENTS.times { Patient.create(name: Faker::Name.name) }
+NUM_PHYSICIANS.times { Physician.create(name: Faker::Name.name) }
+
+prng = Random.new
+APPOINTMENTS.times do
+  physician_id = prng.rand(NUM_PHYSICIANS)
+  patient_id = prng.rand(NUM_PATIENTS)
+  fake_date = Faker::Time.between(DateTime.now, DateTime.now + 365)
+  Appointment.create(
+    appointment_date: fake_date,
+    physician_id: physician_id,
+    patient_id: patient_id
+  )
+end
